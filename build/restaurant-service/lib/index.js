e17cb51eb98b95e21acc463c66f4a59611f46c67
zoharngo <zoharngo@gmail.com>
2018-11-01 12:56:32 +0200

    procfile v

--- build/restaurant-service/lib/index.js
+++ build/restaurant-service/lib/index.js
@@ -17,14 +17,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 class RestaurantService {
   constructor() {
     this.postRestaurants = async rowData => {
-      const res = await _axios2.default.post(`http://${_config2.default.API_HOST}:${_config2.default.API_PORT}/api`, rowData);
+      const res = await _axios2.default.post(`https://${_config2.default.API_HOST}/api`, rowData);
       const { data } = await res;
       return data;
     };
 
     this.saveRestaurant = async restaurant => {
       const uuid = restaurant['uuid'];
-      const res = await _axios2.default.put(`http://${_config2.default.API_HOST}:${_config2.default.API_PORT}/api/${uuid}`, { restaurant_name: restaurant.restaurant_name,
+      const res = await _axios2.default.put(`https://${_config2.default.API_HOST}/api/${uuid}`, { restaurant_name: restaurant.restaurant_name,
         restaurant_type: restaurant.restaurant_type,
         phone: restaurant.phone
       });
@@ -34,19 +34,19 @@ class RestaurantService {
     };
 
     this.getRestaurants = async () => {
-      const res = await _axios2.default.get(`http://${_config2.default.API_HOST}:${_config2.default.API_PORT}/api`);
+      const res = await _axios2.default.get(`https://${_config2.default.API_HOST}/api`);
       const { data } = await res;
       return data;
     };
 
     this.getRestaurant = async uuid => {
-      const res = await _axios2.default.get(`http://${_config2.default.API_HOST}:${_config2.default.API_PORT}/api/${uuid}`);
+      const res = await _axios2.default.get(`https://${_config2.default.API_HOST}/api/${uuid}`);
       const { data } = await res;
       return data;
     };
 
     this.deleteRestaurant = async uuid => {
-      await _axios2.default.delete(`http://${_config2.default.API_HOST}:${_config2.default.API_PORT}/api/${uuid}`);
+      await _axios2.default.delete(`http://${_config2.default.API_HOST}/api/${uuid}`);
     };
 
     this.getRestaurantLocationByCords = async (lat, lng) => {
